7e892eade1bc8f2c57516d75697ed30f14c93b89
Anubhav Goel <anubhavgoel0520@hotmail.com>
2025-06-04 20:37:51 +0530

    Add and update configuration classes and settings

---
+++ devconnect-backend/src/main/java/com/devconnect/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.devconnect.config;
+
+public class JwtConstant {
+	public static String JWT_HEADER = "Authorization";
+	public static String SECRET_KEY = "knadjksbjasdbjJKBCIHsbhjbIJB893YQRUBjkbIUGDEUIQHG39";
+}
